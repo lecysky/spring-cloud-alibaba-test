bcaec251a2dc364421e6df6cfb66cb885bc1b260
lecy <274525279@qq.com>
2021-09-08 09:29:35 +0800

    提交模块创建

---
+++ shopping-center/shop-product/src/main/java/com/product/ShopProductApplication.java
@@ -0,0 +1,13 @@
+package com.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ShopProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopProductApplication.class,args);
+    }
+}
